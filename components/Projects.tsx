cc1244be4b253d8c18d6f4193446b91463cb7d89
Marwan Mostafa <Marwan.morsi@outlook.com>
2023-01-11 19:34:53 +0200

    Projects section

---
+++ components/Projects.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+type Props = {};
+
+const Projects = (props: Props) => {
+  const projects = [1, 2, 3, 4, 5];
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="relative h-screen flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0"
+    >
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Projects
+      </h3>
+
+      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+        {projects.map((project, i) => (
+          <div
+            key={i}
+            className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen"
+          >
+            <motion.img
+              initial={{ y: -300, opacity: 0 }}
+              whileInView={{ y: 0, opacity: 1 }}
+              transition={{ duration: 1.2 }}
+              viewport={{ once: true }}
+              src="marwan.png"
+              alt="project"
+            />
+            <div className="space-y-10 px-0 md:px-10 max-w-6xl">
+              <h4 className="text-4xl font-semibold text-center">
+                <span className="underline decoration-[#FF0266]/50">
+                  App {i + 1} of {projects.length}:
+                </span>{' '}
+                Taking Notes App
+              </h4>
+
+              <p className="text-lg text-center md:text-left">
+                Bacon ipsum dolor amet swine chislic short ribs
+                turducken. Leberkas jowl burgdoggen turducken,
+                meatball short ribs ball tip. Kielbasa doner leberkas
+                spare ribs ball tip sirloin flank tongue, brisket
+                burgdoggen. Ham pork chop strip steak shoulder brisket
+                cow bacon.Bacon ipsum dolor amet swine chislic short
+                ribs turducken. Leberkas jowl burgdoggen turducken,
+                meatball short ribs ball tip. Kielbasa doner leberkas
+                spare ribs ball tip sirloin flank tongue, brisket
+                burgdoggen. Ham pork chop strip steak shoulder brisket
+                cow bacon.
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="w-full absolute top-[30%] bg-[#FF0266]/10 left-0 h-[500px] -skew-y-12" />
+    </motion.div>
+  );
+};
+
+export default Projects;
