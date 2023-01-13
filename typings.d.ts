864c12f8f3bc1ec3c7dc5fefa0f429862e975fb6
Marwan Mostafa <Marwan.morsi@outlook.com>
2023-01-13 02:43:46 +0200

    adding types

--- typings.d.ts
+++ typings.d.ts
@@ -13,6 +13,53 @@ interface Image {
   };
 }
 
+export interface pageInfo extends SanityBody {
+  _type: 'pageInfo';
+  name: string;
+  backgroundInformation: string;
+  role: string;
+  address: string;
+  email: string;
+  phoneNumber: string;
+  profilePic: Image;
+  heroImage: Image;
+}
+
+export interface Technology extends SanityBody {
+  _type: 'skill';
+  image: Image;
+  progress: number;
+  titles: string;
+}
+
+export interface Skill extends SanityBody {
+  _type: 'skill';
+  image: Image;
+  progress: number;
+  titles: string;
+}
+
+export interface Experience extends SanityBody {
+  _type: 'experience';
+  company: string;
+  companyImage: Image;
+  dateStarted: date;
+  dateEnded: date;
+  isCurrentlyWorking: boolean;
+  jobTitle: string;
+  points: string[];
+  technologies: Technology[];
+}
+
+export interface Project extends SanityBody {
+  _type: 'project';
+  titles: string;
+  image: Image;
+  linkToBuild: string;
+  summary: string;
+  technology: Technology[];
+}
+
 export interface Social extends SanityBody {
   _type: 'social';
   title: string;
